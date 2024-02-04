4003b846459ca58c11d646ed9e99597229d83a8c
_kelvin_real_ <120066313+kelvin31272@users.noreply.github.com>
2024-02-04 16:08:17 +0000

    New main folder, run .exe

--- windows-amd64/source/EVOLUTION_DEMO.java
+++ windows-amd64/source/EVOLUTION_DEMO.java
@@ -826,13 +826,14 @@ class Enemy {
 
   public void Run() {
     //this is for following the mouse
-    // float x = (mouseX - width/2 ) / CAMERA_ZOOM + CAMERA_X, y = (mouseY - height/2 ) / CAMERA_ZOOM  + CAMERA_Y;
-    float x = enemyTrack[currentTrack][0];
-    float y = enemyTrack[currentTrack][1];
-    if (isWithinError(enemyX,enemyTrack[currentTrack][0],10) && isWithinError(enemyY,enemyTrack[currentTrack][1],10)) {
-      currentTrack++;
-      currentTrack = constrain(currentTrack,0,enemyTrack.length-1);
-    }
+     float x = (mouseX - width/2 ) / CAMERA_ZOOM + CAMERA_X;
+     float y = (mouseY - height/2 ) / CAMERA_ZOOM  + CAMERA_Y;
+    //float x = enemyTrack[currentTrack][0];
+    //float y = enemyTrack[currentTrack][1];
+    //if (isWithinError(enemyX,enemyTrack[currentTrack][0],10) && isWithinError(enemyY,enemyTrack[currentTrack][1],10)) {
+    //  currentTrack++;
+    //  currentTrack = constrain(currentTrack,0,enemyTrack.length-1);
+    //}
     PVector dm = new PVector(x - enemyX, y -enemyY );
     vel = PVector.fromAngle(dm.heading());
     vel.setMag(enemySpeed);    
